b6ebed348a5ecf520d26b7008a89a7e9c2120fb2
Ronaldo e Silva Vieira <ronaldo.vieira@dcc.ufmg.br>
2022-03-13 19:30:25 -0300

    Configure Swagger UI

--- src/main/java/app/web/bozenaapi/BozenaApiApplication.java
+++ src/main/java/app/web/bozenaapi/BozenaApiApplication.java
@@ -1,13 +1,28 @@
 package app.web.bozenaapi;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BozenaApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BozenaApiApplication.class, args);
+    }
+
+    @Bean
+    public OpenAPI bozenaOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Bozena API")
+                        .description("Bozena is an open-source tournament manager API built for Taça Bozena.")
+                        .version("0.0.1-SNAPSHOT")
+                        .license(new License().name("MIT"))
+                );
     }
 
 }
